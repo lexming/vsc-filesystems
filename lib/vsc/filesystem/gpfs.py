2d6cda0659c1460a666d73f07514bc1a02ca0685
Alex Domingo <alex.domingo.toro@vub.be>
2025-09-18 10:56:20 +0200

    set --allow-permission-change=chmodAndUpdateAcl on new filesets in GPFS

--- lib/vsc/filesystem/gpfs.py
+++ lib/vsc/filesystem/gpfs.py
@@ -816,6 +816,11 @@ class GpfsOperations(PosixOperations, metaclass=Singleton):
                     GpfsOperationError)
             mmcrfileset_options += ['--inode-space', parent_fileset_name]
 
+        # Enable update of ACL permissions on chmod commands in all cases
+        # default setting is chmodAndSetAcl, which makes chmod update POSIX ACLs, but fully replace NFS4 ACLs
+        # by swithcing to chmodAndUpdateAcl, chmod behaves in the same way (updates) both ACL types
+        mmcrfileset_options += ['--allow-permission-change', 'chmodAndUpdateAcl']
+
         (ec, out) = self._execute('mmcrfileset', mmcrfileset_options, True)
         if ec > 0:
             self.log.raiseException(
